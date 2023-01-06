eb36148a51964233962dd8495db54ab56dc9bfa6
Rince Mathew <rincemathewmanattu@gmail.com>
2023-01-06 14:13:48 +0530

    intial commit

---
+++ assigments/1accept.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    char text;
+    // for int %d, for float %f, for character %c
+    // %d,%f,%c etc these are called escape characters
+    printf("Enter some text ");
+    printf("Enter some text ");
+    scanf("%c", &text);
+    printf("you have entered %c", text);
+
+    return 0;
+}
+
+
+
+
+// //for more than one char
+// // Online C compiler to run C program online
+// #include <stdio.h>
+
+// int main() {
+//     char text[50];
+//     // for int %d, for float %f, for character %c
+//     // %d,%f,%c etc these are called escape characters
+//     printf("Enter some text ");
+//     scanf("%s", &text);
+//     printf("you have entered %s", text);
+
+//     return 0;
+// }
